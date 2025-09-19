66a7c4166c491a56c389ddaf509f88cefc8e4bed
SaraToth <saratoth101@gmail.com>
2025-09-19 15:57:56 +0900

    Feat: Fix verifyToken logic

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,5 +1,5 @@
-const verifyToken = (req, res, next) => {
-    next()
-};
+const passport = require("../config/passport");
+
+const verifyToken = passport.authenticate("jwt", { session: false });
 
 module.exports = verifyToken;
