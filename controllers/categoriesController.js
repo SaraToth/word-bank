0029002f4a42dc189fad767acfbf05b02b3beafc
SaraToth <saratoth101@gmail.com>
2025-09-19 14:00:26 +0900

    Feat: Begin categoriesController logic and jsDoc

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,15 +1,51 @@
+const prisma = require("../prisma/client");
+
+// Type definitions
+/**
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Response} Response
+*/
+
+/**
+ * Gets a list of user's existing categories
+ * 
+ * @param {Request} req 
+ * @param {Response} res
+ * @returns {Promise<Response>} JSON response:
+ */
 const getCategories = (req, res) => {
 
 };
 
+/**
+ * Creates a new category for current user
+ * 
+ * @param {Request} req 
+ * @param {Response} res
+ * @returns {Promise<Response>} JSON response:
+ */
 const postCategories = (req, res) => {
 
 };
 
+/**
+ * Renames one of the user's existing categories
+ * 
+ * @param {Request} req 
+ * @param {Response} res
+ * @returns {Promise<Response>} JSON response:
+ */
 const patchCategories = (req, res) => {
 
 };
 
+/**
+ * Deletes a user's category
+ * 
+ * @param {Request} req 
+ * @param {Response} res
+ * @returns {Promise<Response>} JSON response:
+ */
 const deleteCategories = (req, res) => {
 
 };
