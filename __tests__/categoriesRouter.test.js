357fef82cc9cc4e538c3efd954bf0640774b4b04
SaraToth <saratoth101@gmail.com>
2025-09-20 15:40:15 +0900

    Test: Finish test for getSingleCategory

--- __tests__/categoriesRouter.test.js
+++ __tests__/categoriesRouter.test.js
@@ -18,7 +18,7 @@ testApp.use(express.json());
 testApp.use(express.urlencoded({ extended: true}));
 
 // Route for testing
-testApp.use("/category", categoriesRouter);
+testApp.use("/categories", categoriesRouter);
 
 // Mock verifyToken passing
 jest.mock("../middleware/verifyToken", () => {
@@ -31,7 +31,7 @@ jest.mock("../middleware/verifyToken", () => {
 describe("GET categories", () => {
     it("Gets the categories for current user", async() => {
         const response = await request(testApp)
-            .get("/category")
+            .get("/categories")
             .expect("Content-Type", /json/)
             .expect(200);
         
@@ -46,4 +46,52 @@ describe("GET categories", () => {
             slug: expect.any(String)
         });
     })
+})
+
+describe("GET single category", () => {
+    it("Gets a single category per the current user's request", async() => {
+        const response = await request(testApp)
+            .get("/categories/1") // Get Ron Weasley's default category
+            .expect("Content-Type", /json/)
+            .expect(200);
+        
+        // Expected response properties:
+        expect(response.body).toHaveProperty("message");
+        expect(response.body).toHaveProperty("category");
+
+        // Expected structure for category:
+        expect(response.body.category).toMatchObject({
+            id: expect.any(Number),
+            name: expect.any(String),
+            slug: expect.any(String)
+        });
+    });
+
+    it("Fails when provided categoryId is not a number", async() => {
+        const response = await request(testApp)
+            .get("/categories/abc")
+            .expect("Content-Type", /json/)
+            .expect(400);
+
+        // Expected response properties:
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it("Fails when user requests a category they don't own", async() => {
+        const response = await request(testApp)
+            .get("/categories/3") // Ron is requesting Hermione's category
+            .expect("Content-Type", /json/)
+            .expect(403);
+        
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it("Fails when user requests a category that doesn't exist", async() => {
+        const response = await request(testApp)
+            .get("/categories/100") // Does not exist
+            .expect("Content-Type", /json/)
+            .expect(404);
+        
+        expect(response.body).toHaveProperty("error");
+    });
 })
