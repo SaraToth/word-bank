8daf8ca605a5e4a5247819dfd80dffdeacfbab5e
SaraToth <saratoth101@gmail.com>
2025-09-21 15:15:52 +0900

    Refactor: getLangPair to processLangPair

---
+++ middleware/processLangPair.js
@@ -0,0 +1,30 @@
+const prisma = require("../prisma/client");
+const asyncHandler = require("express-async-handler");
+
+const processLangPair = asyncHandler( async(req, res, next) => {
+
+    // Gets the language pair from path
+    const pairId = parseInt(req.params.pairId);
+
+
+    // Confirm it exists
+    if (!pairId || Number.isNaN(pairId)) {
+        return res.status(400).json({ error: "Either language pair is missing or invalid"});
+    }
+
+    // Access language pair in db
+    const langPair = await prisma.language.findUnique({
+        where: { id: pairId}
+    });
+
+    // Check language pair exists
+    if (langPair === null) {
+        return res.status(404).json({ message: "Language pair does not exist"});
+    }
+
+    req.pairId = langPair.id;
+
+    next();
+});
+
+module.exports = processLangPair;
