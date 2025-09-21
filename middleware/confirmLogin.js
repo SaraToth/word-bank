020289c0ad2650603eb121fdc2b6237187e5ad5a
SaraToth <saratoth101@gmail.com>
2025-09-21 15:40:09 +0900

    Docs: Update jsdoc for confirmLogin

--- middleware/confirmLogin.js
+++ middleware/confirmLogin.js
@@ -1,3 +1,20 @@
+// Type definitions
+/**
+ * @typedef {import("express").Request} Request
+ * @typedef {import("express").Response} Response
+ * @typedef {import("express").NextFunction } NextFunction
+*/
+
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res
+ * @param { NextFunction } next
+ * @returns {void} JSON Response:
+ *  - JSON Respone: 401 { error: String} - if user's id (from jwt) does not exist
+ *  - calls Next if userId exists
+ *  
+ */
 const confirmLogin = (req, res, next) => {
     // Access current user id from json web token
     const userId = parseInt(req.user.id);
