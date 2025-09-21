ab8f8a4533b014e080637e987ca752fa84ad26eb
SaraToth <saratoth101@gmail.com>
2025-09-21 20:29:14 +0900

    Feat: Finish addWord logic

--- controllers/wordsController.js
+++ controllers/wordsController.js
@@ -1,6 +1,7 @@
 const prisma = require("../prisma/client");
 const asyncHandler = require("express-async-handler");
 const { validationResult } = require("express-validator");
+const validateWord = require("../utils/validateWord");
 const slugifyText = require("../utils/slugifyText");
 
 const getWords = asyncHandler( async(req, res) => {
@@ -49,4 +50,47 @@ const getWords = asyncHandler( async(req, res) => {
     
 })
 
-module.exports = { getWords };
+const addWord = [
+    validateWord,
+
+    asyncHandler( async(req, res) => {
+        // Handle valiation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+
+        // Access input from form:
+        const { l1Word, l2Word, example } = req.body;
+
+        // Confirm l1word doesn't already exist
+        const check = await prisma.word.findFirst({
+            where: { l1Word: l1Word}
+        })
+        if (check) {
+            return res.status(400).json({ error: "Bad request: that word is already in your dictionary"});
+        }
+
+        // Create the new word
+        const newWord = await prisma.word.create({
+            data: {
+                userId: req.userId,
+                languageId: req.pairId,
+                l1Word: l1Word,
+                l2Word: l2Word,
+                example: example? example : null,
+                categories: {
+                    connect: { id: parseInt(req.params.categoryId) }
+                }
+            },
+            select: { id: true, l1Word: true, l2Word: true, example: true }
+        });
+
+        return res.status(200).json({
+            message: `Succesfully added ${l1Word} to current category`,
+            word: newWord
+        });
+    })
+];
+
+module.exports = { getWords, addWord };
--- routes/wordsRouter.js
+++ routes/wordsRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const wordsRouter = Router({ mergeParams: true });
 const verifyToken = require("../middleware/verifyToken");
-const { getWords } = require("../controllers/wordsController");
+const { getWords, addWord } = require("../controllers/wordsController");
 
 // Protect routes with user authentication
 wordsRouter.use(verifyToken);
 
 wordsRouter.get("/", getWords);
+wordsRouter.post("/", addWord);
 
 module.exports = wordsRouter;
