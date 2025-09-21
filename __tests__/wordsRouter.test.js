a8bb0de2bd1d51ea1e77b460f670c5754e606038
SaraToth <saratoth101@gmail.com>
2025-09-21 20:29:30 +0900

    Test: Finish test for addWord

--- __tests__/wordsRouter.test.js
+++ __tests__/wordsRouter.test.js
@@ -31,6 +31,11 @@ testApp.use("/languages", languagesRouter);
 testApp.use("/languages/:pairId/categories", categoriesRouter)
 testApp.use("/languages/:pairId/categories/:categoryId/words", wordsRouter);
 
+testApp.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err.message });
+});
+
 describe("GET words", () => {
 
     it("Gets words", async() => {
@@ -66,6 +71,80 @@ describe("GET words", () => {
     // })
 }) 
 
+describe("POST add word", () => {
+    it("Fails if validation fails", async() => {
+        const response = await request(testApp)
+            .post("/languages/1/categories/1/words")
+            .send({
+                l1Word: "안녕하세요",
+                l2Word: "", // Required data missing
+                example: ""
+            })
+            .expect("Content-type", /json/) 
+            .expect(400)
+
+        expect(response.body).toHaveProperty("errors")
+        expect(Array.isArray(response.body.errors)).toBe(true);
+    })
+
+    it("Succesfully creates a new word", async() => {
+        const response = await request(testApp)
+            .post("/languages/1/categories/1/words")
+            .send({
+                l1Word: "hello",
+                l2Word: "안녕하세요",
+                example: "안녕하세요, 저는 세라입니다."
+            })
+            .expect("Content-type", /json/)
+            .expect(200);
+        
+        // Expected response properties:
+        expect(response.body).toHaveProperty("message");
+        expect(response.body).toHaveProperty("word");
+
+        // Expected word structure:
+        expect(response.body.word).toMatchObject({
+            id: expect.any(Number),
+            l1Word: expect.any(String),
+            l2Word: expect.any(String),
+            example: expect.any(String)
+        });
+
+        // Delete when test ends
+        await prisma.word.delete({
+            where: {id: response.body.word.id}
+        });
+    });
+
+    it("Succesfully creates a new word with no example", async() => {
+        const response = await request(testApp)
+            .post("/languages/1/categories/1/words")
+            .send({
+                l1Word: "fruit",
+                l2Word: "과일",
+                example: "" // Empty should be allowed
+            })
+            .expect("Content-type", /json/)
+            .expect(200);
+        
+        // Expected response properties:
+        expect(response.body).toHaveProperty("message");
+        expect(response.body).toHaveProperty("word");
+
+        // Expected word structure:
+        expect(response.body.word).toMatchObject({
+            id: expect.any(Number),
+            l1Word: expect.any(String),
+            l2Word: expect.any(String),
+            example: null
+        });
+        
+        // Delete when test ends
+        await prisma.word.delete({
+            where: {id: response.body.word.id}
+        });
+    })
+})
 afterAll(async () => {
   await prisma.$disconnect();
 });
