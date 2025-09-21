a9dc5cad0eb9a28a3f4f9f2af733326ab10a1064
SaraToth <saratoth101@gmail.com>
2025-09-21 20:28:44 +0900

    Feat: validateWord checks for ALL alphabets

--- utils/validateWord.js
+++ utils/validateWord.js
@@ -1,23 +1,40 @@
 const { body } = require("express-validator");
-const prisma = require("../prisma/client");
-const toProperNoun = require("./toProperNoun");
 
 const validateWord = [
     body("l1Word")
         .trim()
-        .notEmpty().withMessage("You must provide a word").bail()
-        .isAlpha().withMessage("Must only contain letters").bail()
-        .isLength({ min: 1, max: 50}).withMessage("Must be no more than 50 characters long").bail()
+        .notEmpty()
+        .withMessage("You must provide a word")
+        .bail()
+        .customSanitizer(value => value.replace(/\s+/g, ' ').trim()) // normalizes spaces due to different alphabets
+        .matches(/^[\p{L}\s]+$/u) // isAlpha check for ALL alphabets
+        .withMessage("Must only contain letters")
+        .bail()
+        .isLength({ min: 1, max: 50})
+        .withMessage("Must be no more than 50 characters long")
+        .bail()
         .toLowerCase(), // Normalizes to lower case
 
     body("l2Word")
         .trim()
-        .notEmpty().withMessage("You must provide a word").bail()
-        .isAlpha().withMessage("Must only contain letters").bail()
-        .isLength({ min: 1, max: 50}).withMessage("Must be no more than 50 characters long").bail()
+        .notEmpty()
+        .withMessage("You must provide a word")
+        .bail()
+        .customSanitizer(value => value.replace(/\s+/g, ' ').trim()) // normalizes spaces due to different alphabets
+        .matches(/^[\p{L}\p{Zs}]+$/u) // isAlpha check for ALL alphabets
+        .withMessage("Must only contain letters")
+        .bail()
+        .isLength({ min: 1, max: 50})
+        .withMessage("Must be no more than 50 characters long")
+        .bail()
         .toLowerCase(), // Normalizes to lower case
     
     body("example")
         .trim()
-        .matches(/^[a-zA-Z0-9 ]+$/).withMessage("Must contain only letters, numbers or spaces")
+        .optional({ checkFalsy: true }) // Only validate when its provided
+        .customSanitizer(value => value.replace(/\s+/g, ' ').trim()) // normalizes spaces due to different alphabets
+        .matches(/^[\p{L}\p{N}\s.,!?:;"'’\-()]+$/u) // isAlpha check for ALL alphabets
+        .withMessage("Example must only contain letters, numbers, spaces, and common punctuation")
 ];
+
+module.exports = validateWord;
