9f1cda1f01cfd2f9563e195fefb9e78c9596e3d5
SaraToth <saratoth101@gmail.com>
2025-09-24 15:03:19 +0900

    Test: Update test for authRouter and languages

--- __tests__/authRouter.test.js
+++ __tests__/authRouter.test.js
@@ -147,6 +147,81 @@ describe("POST login", () => {
     })
 })
 
+describe("POST setup language", () => {
+    it("Fails when validation data is missing", async() => {
+        const response = await request(testApp)
+            .post("/languages")
+            .send({
+                langOne: "",
+                langTwo: ""
+            })
+            .expect("Content-type", /json/)
+            .expect(400);
+        
+        // Expected response property and structure:
+        expect(response.body).toHaveProperty("errors");
+        expect(Array.isArray(response.body.errors)).toBe(true);
+    });
+
+    it("Fails when user chooses same language", async() => {
+        const response = await request(testApp)
+            .post("/languages")
+            .send({
+                langOne: "EN",
+                langTwo: "EN"
+            })
+            .expect("Content-type", /json/)
+            .expect(400);
+        
+            // Expected response property and structure:
+            expect(response.body).toHaveProperty("errors");
+            expect(Array.isArray(response.body.errors)).toBe(true);
+    });
+
+    it("Fails when language pair isn't yet offered in db", async() => {
+        const response = await request(testApp)
+            .post("/languages")
+            .send({
+                langOne: "FR",
+                langTwo: "EN"
+            })
+            .expect("Content-type", /json/)
+            .expect(404);
+
+        expect(response.body).toHaveProperty("error");
+    });
+
+    it("Succeeds", async() => {
+        const response = await request(testApp)
+            .post("/languages")
+            .send({
+                langOne: "EN",
+                langTwo: "FR"
+            })
+            .expect("Content-type", /json/)
+            .expect(200);
+
+        // Expected response properties
+        expect(response.body).toHaveProperty("message");
+        expect(response.body).toHaveProperty("category");
+        
+        // Expected category object:
+        expect(response.body.category).toMatchObject({
+            id: expect.any(Number),
+            name: expect.any(String),
+            slug: expect.any(String)
+        });
+
+        // Expect the default category name:
+        expect(response.body.category.name).toBe("My Words");
+
+        // Delete the category from test
+        await prisma.category.delete({
+            where: { id: response.body.category.id }
+        });
+    });
+});
+
 afterAll(async () => {
   await prisma.$disconnect();
 });
