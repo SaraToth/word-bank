3fe477bae9ff6b7af5cf11c2fd54f9d5e68f185a
SaraToth <saratoth101@gmail.com>
2025-09-25 13:36:56 +0900

    Refactor: words routes to depend on authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,19 +1,26 @@
 const { Router } = require("express");
 const authRouter = Router();
 const verifyToken = require("../middleware/verifyToken");
-
-const { postSignup, postLogin, setUpLanguage } = require("../controllers/authController");
 const confirmLogin = require("../middleware/confirmLogin");
+const processLangPair = require("../middleware/processLangPair");
+const { postSignup, postLogin, setUpLanguage } = require("../controllers/authController");
+const categoriesRouter = require("../routes/categoriesRouter");
+const wordsRouter = require("../routes/wordsRouter");
 
-// Signup
+// Routes
 authRouter.post("/signup", postSignup);
-
-// Login
 authRouter.post("/login", postLogin);
 
-// Add a language to user
+// Protect the following routes with authentification
 authRouter.use(verifyToken);
-authRouter.use(confirmLogin);
+authRouter.use(confirmLogin); // passes token's userId to req.userId
+
+// Subroutes:
+authRouter.use("/languages/:languagesSlug/categories", processLangPair, categoriesRouter);
+authRouter.use("/languages/:languagesSlug/words", processLangPair, wordsRouter);
+
+// Protected Routes
 authRouter.post("/languages", setUpLanguage);
+
 
 module.exports = authRouter;
--- routes/languagesRouter.js
+++ routes/languagesRouter.js
@@ -1,20 +1,9 @@
 const { Router } = require("express");
 const languagesRouter = Router();
-const verifyToken = require("../middleware/verifyToken");
-const categoriesRouter = require("../routes/categoriesRouter");
-const { setUpLanguage, getLanguageCodes } = require("../controllers/languagesController");
-const processLangPair = require("../middleware/processLangPair");
-const confirmLogin = require("../middleware/confirmLogin");
-const wordsRouter = require("./wordsRouter");
+const { getLanguageCodes } = require("../controllers/languagesController");
 
-// Protect routes with user authentication
-languagesRouter.use(verifyToken);
-languagesRouter.use(confirmLogin);
-
-// Subroutes:
-languagesRouter.use("/:languagesSlug/categories", processLangPair, categoriesRouter);
-languagesRouter.use("/:languagesSlug/words", wordsRouter);
-
+// Gets available language codes
 languagesRouter.get("/", getLanguageCodes)
+
 
 module.exports = languagesRouter;
