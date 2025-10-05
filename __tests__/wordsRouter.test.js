3f3ffd37f9a64f8b14f959a2026cdff1934dfb75
SaraToth <saratoth101@gmail.com>
2025-10-05 18:09:35 +0900

    Test: Finalize test for adding words

--- __tests__/wordsRouter.test.js
+++ __tests__/wordsRouter.test.js
@@ -85,23 +85,19 @@ describe("POST add word", () => {
                 }]
             })
             .expect("Content-type", /json/)
-            .expect(200);
+            .expect(201);
         
         // Expected response properties:
         expect(response.body).toHaveProperty("message");
-        expect(response.body).toHaveProperty("word");
 
-        // Expected word structure:
-        expect(response.body.word).toMatchObject({
-            id: expect.any(Number),
-            l1Word: expect.any(String),
-            l2Word: expect.any(String),
-            example: expect.any(String)
+        // Get new word
+        const newWord = await prisma.word.findFirst({
+            where: { l2Word: "안녕하세요", l1Word: "hello", userId: 1}
         });
 
         // Delete when test ends
         await prisma.word.delete({
-            where: {id: response.body.word.id}
+            where: { id: newWord.id}
         });
     });
 
@@ -117,24 +113,71 @@ describe("POST add word", () => {
                 }]
             })
             .expect("Content-type", /json/)
-            .expect(200);
+            .expect(201);
         
         // Expected response properties:
         expect(response.body).toHaveProperty("message");
-        expect(response.body).toHaveProperty("word");
-
-        // Expected word structure:
-        expect(response.body.word).toMatchObject({
-            id: expect.any(Number),
-            l1Word: expect.any(String),
-            l2Word: expect.any(String),
-            example: null
-        });
         
+        // Get new wordId
+        const word = await prisma.word.findFirst({
+            where: { l1Word: "fruit", l2Word: "과일"}
+        });
         // Delete when test ends
         await prisma.word.delete({
-            where: {id: response.body.word.id}
+            where: { id: word.id}
         });
+    })
+
+    it("Creates a new category if it doesn't exist", async() => {
+        const categories = await prisma.category.findMany({
+            where: { languageId: 1, userId: 1}
+        });
+        expect(categories.length).toBe(1);
+
+        const response = await request(testApp)
+            .post("/user/languages/en-kr/categories/1/words")
+            .send({
+                words: [{
+                    l1Word: "fruit",
+                    l2Word: "과일",
+                    example: "", // Empty should be allowed
+                    categories: ["food"]
+                }]
+            })
+            .expect("Content-type", /json/)
+            .expect(201);
+        
+        // Success message response:
+        expect(response.body).toHaveProperty("message");
+
+        // Access the category that should have been created:
+        const newCategory = await prisma.category.findFirst({
+            where: { name: "Food"}
+        });
+
+        expect(newCategory).toBeDefined();
+
+        // Access newly created word:
+        const word = await prisma.word.findFirst({
+            where: {
+                l1Word: "fruit"
+            }
+        });
+
+        // Delete the word first
+        await prisma.word.delete({
+            where: {
+                id: word.id
+            }
+        });
+
+        // Delete the category
+        await prisma.category.delete({
+            where: {
+                id: newCategory.id
+            }
+        })
+
     })
 })
 
@@ -150,12 +193,25 @@ describe("POST bulk add words", () => {
 
         expect(response.body).toHaveProperty("message");
         
-        const l2Words = bulkWords.map(w => w.l2Word);
+        let wordIds = [];
+        for (const word of bulkWords) {
+
+            const addedWord = await prisma.word.findFirst({
+                where: {
+                    userId: 1,
+                    l2Word: word.l2Word,
+                    l1Word: word.l1Word
+                }
+            });
+            
+            if (addedWord) {
+                wordIds.push(addedWord.id)
+            }
+        };
 
         await prisma.word.deleteMany({
             where: {
-                l2Word: { in: l2Words },
-                userId: 1,
+                id: { in: wordIds },
             }
         });
     })
@@ -179,6 +235,68 @@ describe("POST bulk add words", () => {
         expect(response.body).toHaveProperty("errors");
         expect(Array.isArray(response.body.errors)).toBe(true);
     });
+
+    it("Creates categories if they don't exist", async() => {
+        // Confirm existing category (default)
+        const oldCategories = await prisma.category.findMany({
+            where: { languageId: 1, userId: 1}
+        });
+        expect(oldCategories.length).toBe(1);
+
+        const response = await request(testApp)
+            .post("/user/languages/en-kr/categories/1/words/bulk")
+            .send({
+                words: [{
+                    l1Word: "tree",
+                    l2Word: "나무",
+                    example: "",
+                    categories: ["nature"]
+                },
+                {
+                    l1Word: "snow",
+                    l2Word: "눈",
+                    example: "",
+                    categories: ["weather", "winter"]
+                }
+            ]
+            })
+            .expect("Content-type", /json/)
+            .expect(200);
+
+        // Response message:
+        expect(response.body).toHaveProperty("message");
+
+        // Get new list of categories:
+        const newCategories = await prisma.category.findMany({
+            where: { userId: 1, languageId: 1}
+        });
+
+        // Confirm categories created:
+        expect(newCategories.length).toBe(4)
+        expect(newCategories.some(c => c.name === "Weather")).toBe(true);
+        expect(newCategories.some(c => c.name === "Nature")).toBe(true);
+        expect(newCategories.some(c => c.name === "Winter")).toBe(true);
+
+        // Get newly created words:
+        const words = await prisma.word.findMany({
+            where: { languageId: 1, userId: 1}  
+        });
+
+        // Delete test words:
+        await prisma.word.deleteMany({
+            where: {
+                id: { in: words.map(w => w.id)}
+            }
+        });
+
+        // Delete test categories:
+        await prisma.category.deleteMany({
+            where: {
+                id: { in: newCategories.map(c => c.id)},
+                type: "CUSTOM"
+            }
+        });
+    })
 })
 
 
